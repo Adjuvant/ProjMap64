6b395d9ce4ad692bcc8ab7e3b68fd8ca55a9050e
Harvey <hmb3141@gmail.com>
2016-06-20 01:45:13 +0100

    Added - script to toggle between gameobject layers

---
+++ Assets/SetActiveLayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetActiveLayer : MonoBehaviour {
+
+    public GameObject[] layers;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) setActiveLayer(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) setActiveLayer(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) setActiveLayer(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) setActiveLayer(3);
+        if (Input.GetKeyDown(KeyCode.Alpha5)) setActiveLayer(4);
+        if (Input.GetKeyDown(KeyCode.Alpha6)) setActiveLayer(5);
+    }
+
+    void setActiveLayer(int i) {
+        foreach (GameObject obj in layers) {
+            obj.SetActive(false);
+        }
+        layers[i].SetActive(true);
+    }
+}
